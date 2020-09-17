664abcf5ed076e77f3f27a6a6045ff549be93a0f
utavegu <utavegu@list.ru>
2020-09-17 16:24:38 +0500

    Рейтинг фильмов - компонент 'Звездочки'

---
+++ src/components/Stars.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Star from './Star'
+import {nanoid} from 'nanoid'
+
+function Stars({count}) {  
+  return (
+    <ul className = "card-body-stars u-clearfix">
+      {(count >= 1 && count <= 5) && (new Array(count).fill(``).map(() => (<li key = {nanoid()}><Star /></li>)))}
+    </ul>
+  )
+}
+
+Stars.propTypes = {
+  count: PropTypes.number.isRequired,
+}
+
+Stars.defaultProps = {
+  count: 0,
+}
+
+export default Stars
+
